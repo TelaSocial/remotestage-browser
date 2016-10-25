6a0f1600f26d78667d88fe4ad9ecfb3d933f1063
Marcio <taboca@gmail.com>
2016-10-25 18:54:22 -0200

    Simplified promises code

--- run.js
+++ run.js
@@ -1,24 +1,23 @@
 var Application = require('spectron').Application;
 var assert = require('assert');
 var electron= require('electron-prebuilt');
+
+// Depends on Promises
 //var Promise = require('es6-promise').Promise;
 
 var app = new Application({
-  path: electron, args:['.']
+	path: electron, args:['.']
 })
 
-var success = function(result) {
-    app.client.waitUntilTextExists('#message', 'success', 10000).then(function () {
-        console.log('success');
-	app.stop();
-    });
-};
-
-var error = function(reason) {
-};
-
-var promise = app.start();
-
-promise.then(success, error);
+app.start().then(function ok() {
+		app.client.waitUntilTextExists('#message', 'success', 10000).then(
+			function success() {
+				console.log('success');
+				app.stop().then(function ok() {
+					console.log('Quit ')
+				});
+			}
+		);
+});
 
 console.log('Launching electron.. and waiting for inner page load!');

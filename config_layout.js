eedfb0ee775b0cb0151a3261a10c4e0a6affd37d
Marcio <taboca@gmail.com>
2016-10-27 15:38:21 -0200

    Layout default

--- config_layout.js
+++ config_layout.js
@@ -1,12 +1,12 @@
 var layout = {
-   'html':'<table width="100%" height="100%"><tr><td class="target" id="c2"></td><td class="target" id="c1"></td></tr></table>',
-   'components': [ 
+   'html':'<table width="100%" height="100%"><tr><td class="target" id="c1"></td><td class="target" id="c2"></td></tr></table>',
+   'components': [
       {'key' : 'c1', 'value': 'http://localhost:8888/static/ping.html'},
-      {'key' : 'c2', 'value': 'http://tmsmmsmlmflsm.fms'},
+      {'key' : 'c2', 'value': 'http://qqqaawww.google.com'},
    ],
-    'components_backup': [ 
+    'components_backup': [
       {'key' : 'c1', 'value': 'http://localhost:8888/static/ping.html'},
-      {'key' : 'c2', 'value': 'http://localhost:8888/static/ping.html'},
+      {'key' : 'c2', 'value': 'http://www.google.com'},
    ],
    'width'  : 640,
    'height' : 480,
